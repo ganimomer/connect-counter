aeb3d14ef87d817ef8aae4640cd3a89235b67858
Omer <ganimomer@gmail.com>
2015-06-19 19:28:25 +0300

    added jade

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Connection counter
 This is an exercise express app that counts how many times a button has been pressed on any browser since the server came up.
 Server side code with Nodejs and Express, client is HTML, CSS and Javascript, using JQuery for Ajax calls and animations.
+
+This branch has an example use of the jade templating engine.
+After a few uses, go to http://localhost:<port>/countPage to see generated content.
--- server/connect-counter.js
+++ server/connect-counter.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var config = require('./config.json');
 var path = require('path');
+var jade = require('jade');
 
 var app = express(),
     counter = 0;
@@ -17,6 +18,10 @@ app.get('/click', function(req, res) {
     console.log('click', counter);
     counter = counter + 1;
     res.status(200).json(counter);
+});
+
+app.get('/countPage', function(req, res) {
+    res.send(jade.render('h1 #{count}',{count: counter}));
 });
 
 app.listen(config.port, function() {
